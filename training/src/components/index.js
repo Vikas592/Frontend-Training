4430fe579a8c1b44c762ffd3e6de94b50daab632
Vikas Choubey <vikas.choubey@successive.tech>
2021-03-19 11:03:56 +0530

    feat: add slider comp

--- training/src/components/index.js
+++ training/src/components/index.js
@@ -2,10 +2,12 @@ import { TextField } from './TextField';
 import { RadioGroup } from './RadioGroup';
 import { SelectField } from './SelectField';
 import { Button } from './Button';
+import { Slider } from './Slider';
 
 export {
   TextField,
   RadioGroup,
   SelectField,
   Button,
+  Slider,
 };
