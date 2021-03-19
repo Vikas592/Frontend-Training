37d8e702878fed1d450f32232b0518ed5460a6bf
Vikas Choubey <vikas.choubey@successive.tech>
2021-03-19 11:02:48 +0530

    feat: add slider

---
+++ training/src/components/Slider/style.js
@@ -0,0 +1,12 @@
+const sliderStyle = {
+  width: '100vw',
+  minHeight: '45vh',
+  padding: '1%',
+  backgroundColor: 'lightgrey',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginBottom: '1%',
+};
+
+export default sliderStyle;
